447bd32196f7ec582cfe074167c6e4ab6923a2d0
rohitdhawadkar <144599518+rohitdhawadkar@users.noreply.github.com>
2024-09-03 18:07:43 +0530

    DLL

--- LinkedList/DoublyLinkedList.cpp
+++ LinkedList/DoublyLinkedList.cpp
@@ -1,103 +1,96 @@
 #include <iostream>
-using namespace std;
 #include <stack>
+using namespace std;
 
 class Node {
-        public:
-        int data;
-        Node* next;
-        Node* Previous;
+public:
+    int data;
+    Node* next;
+    Node* previous;
 
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+        this->previous = nullptr;
+    }
 
-                        Node(int data){
-                    this->data = data;
-                    this->next = nullptr;
-                    this->Previous = nullptr;
-                }
-
-Node(int data,Node* next,Node* Previous){
+    Node(int data, Node* next, Node* previous) {
         this->data = data;
         this->next = next;
-        this->Previous = Previous;
+        this->previous = previous;
     }
-
-
 };
 
-Node* convertAll2Arr(int array[],int size);
-void printDLL(Node* Head);
-void freeLinkedList(Node* Head);
-int main(){
-int array[] = {55,97,89,56,67};
-Node* H = convertAll2Arr(array,5);
-printDLL(H);
-freeLinkedList(H);
-return 0;
+Node* convertArr2DLL(int array[], int size);
+void printDLL(Node* head);
+void freeLinkedList(Node* head);
+Node* reverseDLL(Node* head);
+Node* swappingDLL(Node* head);
 
-
+int main() {
+    int array[] = {55, 97, 89, 56, 67};
+    Node* head = convertArr2DLL(array, 5);
+    printDLL(head);
+    freeLinkedList(head);
+    return 0;
 }
 
-
-Node* convertAll2Arr(int array[],int size){
-    Node* Head = new Node(array[0]);
-    Node* prev = Head;
-    for(int i=1;i<size;i++){
+Node* convertArr2DLL(int array[], int size) {
+    Node* head = new Node(array[0]);
+    Node* prev = head;
+    for (int i = 1; i < size; i++) {
         Node* temp = new Node(array[i]);
         prev->next = temp;
-        temp->Previous = prev;
+        temp->previous = prev;
         prev = prev->next;
     }
-
-    return Head;
+    return head;
 }
 
-void printDLL(Node* Head){
-    Node* temp = Head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
+void printDLL(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
         temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void freeLinkedList(Node* Head) {
+void freeLinkedList(Node* head) {
     Node* temp;
-
-    while (Head != nullptr) {
-        temp = Head;
-        Head = Head->next;
+    while (head != nullptr) {
+        temp = head;
+        head = head->next;
         delete temp;
     }
 }
 
-Node* reverseDLL(Node* Head){
-    Node* temp = Head;
+Node* reverseDLL(Node* head) {
+    Node* temp = head;
     stack<int> tempStack;
 
-    while (temp!=NULL) {
+    while (temp != nullptr) {
         tempStack.push(temp->data);
-        temp= temp->next;
-
+        temp = temp->next;
     }
 
-    temp = Head;
-while(temp!=NULL){
-    temp->data = tempStack.top();
-    tempStack.pop();
-    temp = temp->next;
-}
-
-return Head;
-}
-
-Node* swappingDDL(Node* Head){
-    Node* Current = Head;
-    while(Current!=NULL){
-        Node* Last = Current->Previous;
-        Current->Previous = Current->next;
-        Current->next = Last;
-        Current = Current->Previous;
+    temp = head;
+    while (temp != nullptr) {
+        temp->data = tempStack.top();
+        tempStack.pop();
+        temp = temp->next;
     }
 
-    return Current->Previous;
+    return head;
+}
+
+Node* swappingDLL(Node* head) {
+    Node* current = head;
+    while (current != nullptr) {
+        Node* last = current->previous;
+        current->previous = current->next;
+        current->next = last;
+        current = current->previous;
+    }
+    return current != nullptr ? current->previous : nullptr;
 }
